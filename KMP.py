c4704f8c0d3842407a1a4f383e1fb037a3cf6460
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2024-10-06 12:51:18 -0400

    frame for program

---
+++ KMP.py
@@ -0,0 +1,17 @@
+'''
+Name: Joseph DiMartino and Zachary Swisher
+Class: MAT-285
+Program: KMP Algorithm
+'''
+
+
+def failureFunction():
+    return
+
+
+def KMP():
+    return
+
+
+pattern = input("Enter a pattern you'd like to search for: ")
+
